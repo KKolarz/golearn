4f3305d6bbe500a0823312b0f675e4848e992017
KKolarz <45627600+KKolarz@users.noreply.github.com>
2026-01-19 16:50:19 +0100

    Finish of the first chapter

--- fundamentals/hello_test.go
+++ fundamentals/hello_test.go
@@ -5,15 +5,29 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("saying hello to your friends", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "eng")
 		want := "Hello, Chris!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to strangers", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, stranger!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in polish", func(t *testing.T) {
+		got := Hello("Krystian", "pol")
+		want := "Dzien dobry, Krystian!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Krystian", "esp")
+		want := "Hola, Krystian!"
 
 		assertCorrectMessage(t, got, want)
 	})
--- fundamentals/main.go
+++ fundamentals/main.go
@@ -1,18 +1,37 @@
 package main
 
-import "fmt"
+const spanish = "esp"
+const polish = "pol"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const polishHelloPrefix = "Dzien dobry, "
+
 const helloSuffix = "!"
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "stranger"
 	}
 
-	return englishHelloPrefix + name + helloSuffix
+	prefix := greetingPrefix(lang)
+
+	return prefix + name + helloSuffix
+}
+
+func greetingPrefix(lang string) (prefix string) {
+
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case polish:
+		prefix = polishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("matt"))
 }
