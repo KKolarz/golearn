12be992249cbc1c2a636ab7d29ce94d823c7fecc
KKolarz <45627600+KKolarz@users.noreply.github.com>
2026-02-04 21:18:27 +0100

    Learned about creating methods and more efficient ways of testing

---
+++ wallet/wallet_test.go
@@ -0,0 +1,53 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("Got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("Wanted an error but received nothig :/")
+		}
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(10)
+		wallet := Wallet{balance: startingBalance}
+
+		err := wallet.Withdraw(Bitcoin(20))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+
+	})
+
+}
